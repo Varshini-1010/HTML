27b63471820b14afb6580b2c4fd2efdc9940e9a1
Varshini-1010 <varshinivelusamy2006@gmail.com>
2025-06-06 15:02:48 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,21 @@
+
+   
+            console.log('hello from script');
+            console.log(5==='5');
+            console.log(5=='5');
+         
+   let i=0
+    do{
+        i++;
+        console.log(i);
+    }while(i<5)    
+        
+function greet(name,age){
+    return 'hello' +" "+ name +" "+'is'+ " "+age + 'old';
+}        
+console.log(greet('ronaldo',35))
+function add(a,b){
+    console.log('a'+'b'+" "+'is',a+b);
+}  
+console.log(add(5675,9415)) 
+ 
